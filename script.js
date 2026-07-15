35d1b10f24f2a19d8c7a6216443ba42ebfe5c53f
Miggyb01 <112913018+Miggyb01@users.noreply.github.com>
2026-07-15 17:35:21 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const correctPassword = " "; //20260615
+const correctBirthday = "20021217";
+
+// Outer Site Password Protection
+function checkPassword(){
+    let input = document.getElementById("password").value;
+    if(input === correctPassword){
+        document.getElementById("password-screen").style.display = "none";
+        document.getElementById("website").style.display = "block";
+        window.scrollTo(0, 0);
+    }
+    else{
+        document.getElementById("error").innerHTML = "❌ Wrong password. Hint: It's our anniversary date!";
+    }
+}
+
+// Inner Birthday Lock inside the Letter Section
+function checkBirthday(){
+    let input = document.getElementById("birthday-input").value;
+    if(input === correctBirthday){
+        // Hide lockbox and smoothly reveal secret letter
+        document.getElementById("letter-lock").style.display = "none";
+        document.getElementById("secret-letter").style.display = "block";
+    }
+    else{
+        document.getElementById("birthday-error").innerHTML = "❌ That doesn't seem to be your birthdate, try again!";
+    }
+}
